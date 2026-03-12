54257c2d39b8b2a9cffb782f8e1b5e2a8db32a44
Matthias Wirth <matthias.wirth@gmail.com>
2026-03-12 16:49:45 +0000

    prom output: fix for "new" autogain

--- rootfs/scripts/generate-prom.sh
+++ rootfs/scripts/generate-prom.sh
@@ -8,12 +8,8 @@ OUT=/run/skyaware978/stats.prom
 TMP=/run/skyaware978/stats.prom.tmp
 
 function generate() {
-    if [[ -f "$AUTOGAIN_CURRENT_VALUE_FILE" ]]; then
-        echo "autogain_current_value=$(cat "$AUTOGAIN_CURRENT_VALUE_FILE")"
-        echo "autogain_max_value=$(cat "$AUTOGAIN_MAX_GAIN_VALUE_FILE")"
-        echo "autogain_min_value=$(cat "$AUTOGAIN_MIN_GAIN_VALUE_FILE")"
-        echo "autogain_pct_strong_messages_max=$AUTOGAIN_PERCENT_STRONG_MESSAGES_MAX"
-        echo "autogain_pct_strong_messages_min=$AUTOGAIN_PERCENT_STRONG_MESSAGES_MIN"
+    if [[ -f "$GAIN_VALUE_FILE" ]]; then
+        echo "autogain_current_value=$(cat "$GAIN_VALUE_FILE")"
     fi
 }
 
